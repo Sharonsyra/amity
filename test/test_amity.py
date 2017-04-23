a1121ac8c0d213adf04baa45d74b7ee027c1bb1d
Sharonsyra <sharonwaithii@gmail.com>
2017-04-23 11:06:13 +0300

    Added more data

--- test/test_amity.py
+++ test/test_amity.py
@@ -13,92 +13,87 @@ class AmityTestCase(unittest.TestCase):
 
     #... Tests for create_room ...#
 
-    def test_create_room_adds_living_space(self):
+    def test_create_room_add_living_space(self):
         """ Test that living space was created successfully """
-        self.assertEqual(self.amity.create_room("Maathai", "Tutu", " Lorup", "LivingSpace"), "Living space has been created")
+        self.assertEqual(self.amity.create_room(["Maathai", "Tutu", " Lorup"], "LivingSpace"), "Living space has been created")
 
-    def test_create_room_adds_office(self):
+    def test_create_room_add_office(self):
         """ Test that office was created successfully """
-        self.assertEqual(self.amity.create_room("PinkRoom", "ConferenceCentre", "BlueRoom", "Office"), "Office has been created")
+        self.assertEqual(self.amity.create_room(["PinkRoom", "ConferenceCentre", "BlueRoom"], "Office"), "Office has been created")
 
-    def test_create_room_can_only_add_room_once(self):
+    def test_create_room_duplicates(self):
         """ Test that room can only be added once """
         self.amity.create_room("Maathai", "LivingSpace")
         self.assertEqual(self.amity.create_room("Maathai", "LivingSpace"), "Room already exists. Create a different room!")
 
-    def test_create_room_cannot_use_same_room_name_for_office_and_living_space(self):
-        """ Test that room name is used for one room type only """
-        self.amity.create_room("PinkRoom", "Office")
-        self.assertEqual(self.amity.create_room("PinkRoom", "LivingSpace"), "The room name is already in use!")
-
-    def test_create_room_rejects_wrong_room_type(self):
+    def test_create_room_wrong_room_type(self):
         """ Test that room type can only be office or living space """
         self.assertEqual(self.amity.create_room("Conference", "Business"), "Business is not a room type! Please try Living Space or Office")
 
     #... Tests for add_person ...#
 
-    def test_add_person_adds_fellow(self):
+    def test_add_person_add_fellow(self):
         """ Test that fellow was added successfully """
-        self.assertEqual(self.amity.add_person("Robley Gori", "Fellow"), "Fellow added successfully!")
+        self.assertEqual(self.amity.add_person("Robley", "Gori", "Fellow", "N"), "Fellow added successfully!")
 
-    def test_add_person_adds_staff(self):
+    def test_add_person_add_staff(self):
         """ Test that staff was added successfully """
-        self.assertEqual(self.amity.add_person("Christina Sass", "Staff"), "Staff added successfully!")
+        self.assertEqual(self.amity.add_person("Christina", "Sass", "Staff", "N"), "Staff added successfully!")
 
-    def test_add_person_adds_person_to_office(self):
+    def test_add_person_add_person_to_office(self):
         """ Test that person is successfully added to office """
         self.amity.create_room("PinkRoom", "Office")
         self.amity.create_room("ConferenceCentre", "Office")
         self.amity.create_room("BlueRoom", "Office")
-        self.assertEqual(self.amity.add_person("Robley Gori", "Fellow", "N"), "Person was allocated an office successfully")
+        self.assertEqual(self.amity.add_person("Robley", "Gori", "Fellow", "N"), "Person was allocated an office successfully")
 
-    def test_add_person_adds_fellow_to_living_space(self):
+    def test_add_person_add_fellow_to_living_space(self):
         """ Test that fellow is successfully added to living space """
         self.amity.create_room("Maathai", "LivingSpace")
         self.amity.create_room("Tutu", "LivingSpace")
         self.amity.create_room("Lorup", "LivingSpace")
-        self.assertEqual(self.amity.add_person("Robley Gori", "Fellow", "Y"), "Fellow was assigned to living space successfully")
+        self.assertEqual(self.amity.add_person("Robley", "Gori", "Fellow", "Y"), "Fellow was assigned to living space successfully")
 
-    def test_add_person_cannot_add_staff_to_living_space(self):
+    def test_add_person_add_staff_to_living_space(self):
         """ Test that staff cannot be added to living space """
-        self.assertEqual(self.amity.add_person("Christina Sass", "Staff", "Y"), "Only fellows can be assigned living space")
+        self.assertEqual(self.amity.add_person("Christina", "Sass", "Staff", "Y"), "Only fellows can be assigned living space")
 
-    def test_add_person_adds_person_to_waiting_list_if_office_is_full(self):
+    def test_add_person_add_person_full_office(self):
         """ Test that person is added to waiting list if offices are full """
         self.amity.create_room("PinkRoom", "Office")
-        self.amity.add_person("Christina Sass", "Staff")
-        self.amity.add_person("Jeremy Johnson", "Staff")
-        self.amity.add_person("Robley Gori", "Fellow", "Y")
-        self.amity.add_person("Jus Machungwa", "Fellow", "N")
-        self.amity.add_person("Angela Mutava", "Fellow", "N")
-        self.amity.add_person("Rose Wambui", "Fellow", "N")
-        self.assertEqual(self.amity.add_person("Wekesa Maina", "Fellow", "Y"), "All the offices are fully occupied at the moment. You have been added to the waiting list")
+        self.amity.add_person("Christina", "Sass", "Staff", "N")
+        self.amity.add_person("Jeremy", "Johnson", "Staff", "N")
+        self.amity.add_person("Robley", "Gori", "Fellow", "Y")
+        self.amity.add_person("Jus", "Machungwa", "Fellow", "N")
+        self.amity.add_person("Angela", "Mutava", "Fellow", "N")
+        self.amity.add_person("Rose" , "Wambui", "Fellow", "N")
+        self.assertEqual(self.amity.add_person("Jackline", "Maina", "Fellow", "Y"), "All the offices are fully occupied at the moment. You have been added to the waiting list")
 
-    def test_add_person_adds_fellow_to_waiting_list_if_living_space_is_full(self):
+    def test_add_person_add_fellow_to_full_living_space(self):
         """ Test that fellow is added to waiting list if living spaces are full """
         self.amity.create_room("Maathai", "LivingSpace")
-        self.amity.add_person("Flevian Kanaiza", "Fellow", "Y")
-        self.amity.add_person("Robley Gori", "Fellow", "Y")
-        self.amity.add_person("Jus Machungwa", "Fellow", "Y")
-        self.amity.add_person("Angela Mutava", "Fellow", "Y")
-        self.assertEqual(self.amity.add_person("Wekesa Maina", "Fellow", "Y"), "Sorry all the living spaces are fully occupied at the moment. You have been added to the waiting list")
+        self.amity.add_person("Flevian", "Kanaiza", "Fellow", "Y")
+        self.amity.add_person("Robley", "Gori", "Fellow", "Y")
+        self.amity.add_person("Jus", "Machungwa", "Fellow", "Y")
+        self.amity.add_person("Angela", "Mutava", "Fellow", "Y")
+        self.assertEqual(self.amity.add_person("Jackline", "Maina", "Fellow", "Y"), "Sorry all the living spaces are fully occupied at the moment. You have been added to the waiting list")
 
-    def test_add_person_adds_fellow_to_living_space_if_office_is_full(self):
+    def test_add_person_add_fellow_to_living_with_full_office(self):
         """ Test that fellow can is added to living space even when offices are full """
         self.amity.create_room("PinkRoom", "Office")
-        self.amity.add_person("Christina Sass", "Staff")
-        self.amity.add_person("Jeremy Johnson", "Staff")
-        self.amity.add_person("Robley Gori", "Fellow", "Y")
-        self.amity.add_person("Jus Machungwa", "Fellow", "N")
-        self.amity.add_person("Angela Mutava", "Fellow", "N")
-        self.amity.add_person("Rose Wambui", "Fellow", "N")
+        self.amity.add_person("Christina", "Sass", "Staff", "N")
+        self.amity.add_person("Jeremy", "Johnson", "Staff", "N")
+        self.amity.add_person("Robley", "Gori", "Fellow", "Y")
+        self.amity.add_person("Jus", "Machungwa", "Fellow", "N")
+        self.amity.add_person("Angela", "Mutava", "Fellow", "N")
+        self.amity.add_person("Rose", "Wambui", "Fellow", "N")
 
         self.amity.create_room("Maathai", "LivingSpace")
-        self.amity.add_person("Flevian Kanaiza", "Fellow", "Y")
-        self.amity.add_person("Robley Gori", "Fellow", "Y")
-        self.amity.add_person("Angela Mutava", "Fellow", "Y")
+        self.amity.add_person("Flevian", "Kanaiza", "Fellow", "Y")
+        self.amity.add_person("Robley", "Gori", "Fellow", "Y")
+        self.amity.add_person("Angela", "Mutava", "Fellow", "Y")
 
-        self.assertEqual(self.amity.add_person("Wekesa Maina", "Fellow", "Y"), "Fellow was assigned a living space successfully. Offices are fully occupied!")
+        self.assertEqual(self.amity.add_person("Wekesa", "Maina", "Fellow", "Y"), "Fellow was assigned a living space successfully. Offices are fully occupied!")
 
     #... Tests for realocate person ...#
 
@@ -107,17 +102,17 @@ class AmityTestCase(unittest.TestCase):
         self.amity.create_room("PinkRoom", "Office")
         self.amity.create_room("ConferenceCentre", "Office")
         self.assertEqual(self.amity.reallocate_person("F1", "PinkRoom"), "Person was reallocated successfully!")
-
+    
     def test_reallocate_person_cannot_reassign__person_to_the_same_room(self):
         """ Tests that person is not reallocated to the same room """
         self.amity.reallocate_person("F1", "Maathai")
         self.assertEqual(self.amity.reallocate_person("F1", "Maathai"), "Person cannot be reallocated to the same room")
-
+    
     def test_reallocate_person_rejects_move_to_room_with_wrong_person_id(self):
         """ Tests that a person cannot be reallocated using a wrong user id """
-        self.amity.add_person("Robley Gori", "Fellow", "F1")
+        self.amity.add_person("Robley", "Gori", "Fellow", "F1")
         self.assertEqual(self.amity.reallocate_person("F9", "Tutu"), "Person cannot be reallocated with wrong person id")
-
+        
     #... Tests for load people ...#
 
     def test_load_people_loads_people_from_txt_file(self):
@@ -129,11 +124,11 @@ class AmityTestCase(unittest.TestCase):
     def test_print_allocations_prints_allocations_to_screen(self):
         """To test if method prints allocations to screen."""
         self.amity.create_room("PinkRoom", "Office")
-        self.amity.add_person("Oliver Munala", "Fellow")
-        self.amity.add_person("Jus Machungwa", "Fellow")
+        self.amity.add_person("Oliver", "Munala", "Fellow", "N")
+        self.amity.add_person("Jus", "Machungwa", "Fellow", "N")
         self.amity.create_room("Maathai", "LivingSpace")
-        self.amity.add_person("Wekesa Maina", "Fellow")
-        self.assertEqual(self.amity.print_allocations(), "PinkRoom - Oliver Munala  Jus Machungwa   Maathai - Wekesa Maina")
+        self.amity.add_person("Jackline", "Maina", "Fellow","N")
+        self.assertEqual(self.amity.print_allocations("Rooms"), "PinkRoom - Oliver Munala  Jus Machungwa   Maathai - Jackline Maina")
 
     def test_print_allocations_prints_allocations_to_txt_file(self):
         """To test if method prints allocations to txt file."""
@@ -144,11 +139,11 @@ class AmityTestCase(unittest.TestCase):
     def test_print_unallocated_prints_unallocated_people_to_screen(self):
         """To test if method prints unallocated people to screen."""
         self.amity.create_room("PinkRoom", "Office")
-        self.amity.add_person("Oliver Munala", "Fellow")
-        self.amity.add_person("Jus Machungwa", "Fellow")
+        self.amity.add_person("Oliver", "Munala", "Fellow", "Y")
+        self.amity.add_person("Jus", "Machungwa", "Fellow", "Y")
         self.amity.create_room("Maathai", "LivingSpace")
-        self.amity.add_person("Wekesa Maina", "Fellow")
-        self.assertEqual(self.amity.print_unallocated(), "PinkRoom - Oliver Munala  Jus Machungwa   Maathai - Wekesa Maina")
+        self.amity.add_person("Jackline", "Maina", "Fellow", "Y")
+        self.assertEqual(self.amity.print_unallocated(), "PinkRoom - Oliver Munala  Jus Machungwa   Maathai - Jackline Maina")
 
     def test_print_unallocated_prints_unallocated_people_to_txt_file(self):
         """To test if method prints unallocated people to txt file."""
@@ -159,11 +154,11 @@ class AmityTestCase(unittest.TestCase):
     def test_print_room_prints_all_people_in_room_name_to_screen(self):
         """ It tests that all people in a room name are printed to screen """
         self.amity.create_room("Tutu", "LivingSpace")
-        self.amity.add_person("Robley Gori", "Fellow", "Y")
-        self.amity.add_person("Wekesa Maina", "Fellow", "Y")
-        self.amity.add_person("Angela Mutava", "Fellow", "Y")
-        self.amity.add_person("Rose Wambui", "Fellow", "Y")
-        self.assertEqual(self.amity.print_room("Tutu"), " Tutu: Robley Gori  Wekesa Maina    Angela Mutava   Rose Wambui")
+        self.amity.add_person("Robley", "Gori", "Fellow", "Y")
+        self.amity.add_person("Jackline", "Maina", "Fellow", "Y")
+        self.amity.add_person("Angela", "Mutava", "Fellow", "Y")
+        self.amity.add_person("Rose", "Wambui", "Fellow", "Y")
+        self.assertEqual(self.amity.print_room("Tutu"), "\n Tutu: \n Robley Gori \n Jackline Maina  \n  Angela Mutava  \n Rose Wambui")
 
     #... Tests for save state ...#
 
@@ -177,10 +172,6 @@ class AmityTestCase(unittest.TestCase):
     def test_load_state_successfully_loads_data_from_database(self):
         """ Test that data is successfully loaded from database """
         self.assertEqual(self.amity.load_state("amity.db"), "Data successfully loaded from database")
-
-    def tearDown(self):
-        self.amity.dispose()
-        self.amity = None
 
 
 if __name__ == '__main__':
