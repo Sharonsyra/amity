7f07cd17e430ddae3c2cd870ba98b499e10a6f50
Sharonsyra <sharonwaithii@gmail.com>
2017-04-23 11:07:03 +0300

    Updated app classes

--- app/amity.py
+++ app/amity.py
@@ -1,24 +1,24 @@
 class Amity():
 
-    def create_room(self):
+    def create_room(self, list_of_rooms, room_type):
         pass
 
-    def add_person(self):
+    def add_person(self, first_name, second_name, type, wants_accomodation):
         pass
 
-    def reallocate_person(self):
+    def reallocate_person(self, person_id, room_name):
         pass
 
-    def load_people(self):
+    def load_people(self, *args):
         pass
 
-    def print_allocations(self):
+    def print_allocations(self, *args):
         pass
 
-    def print_unallocated(self):
+    def print_unallocated(self, *args):
         pass
 
-    def print_room(self):
+    def print_room(self, room):
         pass
 
     def get_available_rooms(self):
@@ -36,8 +36,8 @@ class Amity():
     def print_people_and_their_allocations(self):
         pass
 
-    def save_state(self):
+    def save_state(self, *args):
         pass
 
-    def load_state(self):
+    def load_state(self, *args):
         pass
--- app/person.py
+++ app/person.py
@@ -1,2 +1,37 @@
-class Person():
-    pass
+class Person(object):
+    def __init__(self, id, names, person_type):
+        self.names = names
+        self.person_type = person_type
+        self.id = id(self)
+
+    def __repr__(self):
+        return "Person {}".format(self.id)
+
+
+class Fellow(Person):
+    def __init__(self, first_name, second_name, person_type="Fellow"):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.person_type = "Fellow"
+
+    def __repr__(self):
+        return "Fellow {0} {1}".format(self.first_name, self.second_name)
+
+
+class Staff(Person):
+    def __init__(self, first_name, second_name, person_type="Staff"):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.person_type = "Staff"
+
+    def __repr__(self):
+        return "Staff {0} {1}".format(self.first_name, self.second_name)
+
+fellows = ["Gyan Mutugi"]
+staff = ["Marlene Mwendwa"]
+newFellow = Fellow("Robley", "Gori")
+newStaff = Staff("Leah", "Mukiri")
+fellows.append(newFellow)
+staff.append(newStaff)
+print(staff)
+print(fellows)
